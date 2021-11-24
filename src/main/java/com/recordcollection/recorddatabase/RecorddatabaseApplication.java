8c9100211202a0387c5c90d282f21fa510d26b0d
Matthew Greco <mgreco2112@tutanota.com>
2021-11-24 16:54:01 -0500

    created Spring package

---
+++ src/main/java/com/recordcollection/recorddatabase/RecorddatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.recordcollection.recorddatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecorddatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecorddatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recordcollection/recorddatabase/RecorddatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recordcollection.recorddatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecorddatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

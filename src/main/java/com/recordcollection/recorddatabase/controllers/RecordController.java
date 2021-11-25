ac2b85380a7cc9c1eeab239216305238c5724274
Matthew Greco <mgreco2112@tutanota.com>
2021-11-25 17:42:06 -0500

    started RecordController methods

--- src/main/java/com/recordcollection/recorddatabase/controllers/RecordController.java
+++ src/main/java/com/recordcollection/recorddatabase/controllers/RecordController.java
@@ -1,11 +1,33 @@
 package com.recordcollection.recorddatabase.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.recordcollection.recorddatabase.models.Record;
+import com.recordcollection.recorddatabase.repositories.ArtistRepository;
+import com.recordcollection.recorddatabase.repositories.RecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/records")
 public class RecordController {
+    @Autowired
+    private RecordRepository repository;
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    @GetMapping
+    public List<Record> getAllRecords() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Record> addRecord(@RequestBody Record record) {
+        return new ResponseEntity<>(repository.save(record), HttpStatus.CREATED);
+    }
+
+
 }

261fd69c1343a96a57a2e9ca386b3154e9cb5f8c
Matthew Greco <mgreco2112@tutanota.com>
2021-11-27 18:50:32 -0500

    added new methods to Collector Controller

--- src/main/java/com/recordcollection/recorddatabase/controllers/CollectorController.java
+++ src/main/java/com/recordcollection/recorddatabase/controllers/CollectorController.java
@@ -1,6 +1,7 @@
 package com.recordcollection.recorddatabase.controllers;
 
 import com.recordcollection.recorddatabase.models.Collector;
+import com.recordcollection.recorddatabase.models.Record;
 import com.recordcollection.recorddatabase.repositories.CollectorRepository;
 import com.recordcollection.recorddatabase.repositories.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -35,6 +37,21 @@ public class CollectorController {
         return repository.save(collector);
     }
 
+    @PostMapping("/record/{id}")
+    public ResponseEntity<Collector> addNewRecordToCollector(@PathVariable Long id, @RequestBody Record record) {
+        Optional<Collector> selCollector = repository.findById(id);
+
+        if (selCollector.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+        recordRepository.save(record);
+
+        selCollector.get().getRecords().add(record);
+
+        return new ResponseEntity<>(repository.save(selCollector.get()), HttpStatus.OK);
+    }
+
     @PutMapping("/{id}")
     public ResponseEntity<Collector> updateCollectorById(@PathVariable Long id, @RequestBody Collector update) {
         Collector selCollector = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
@@ -51,4 +68,12 @@ public class CollectorController {
         return new ResponseEntity<>(repository.save(selCollector), HttpStatus.OK);
     }
 
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable Long id) {
+        Collector delCollector = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        repository.delete(delCollector);
+
+        return new ResponseEntity<>("Collector Deleted", HttpStatus.OK);
+    }
 }

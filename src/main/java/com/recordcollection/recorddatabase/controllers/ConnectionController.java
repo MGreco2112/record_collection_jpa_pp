f4fcf0f7f2b978c2679f1bff4b90fc1c559dc490
Matthew Greco <mgreco2112@tutanota.com>
2022-01-15 17:20:10 -0500

    created get mapping

--- src/main/java/com/recordcollection/recorddatabase/controllers/ConnectionController.java
+++ src/main/java/com/recordcollection/recorddatabase/controllers/ConnectionController.java
@@ -1,12 +1,23 @@
 package com.recordcollection.recorddatabase.controllers;
 
+import com.recordcollection.recorddatabase.connections.Connection;
+import com.recordcollection.recorddatabase.connections.EConnection;
+import com.recordcollection.recorddatabase.models.Collector;
+import com.recordcollection.recorddatabase.models.auth.User;
+import com.recordcollection.recorddatabase.payloads.response.MessageResponse;
 import com.recordcollection.recorddatabase.repositories.CollectorRepository;
 import com.recordcollection.recorddatabase.repositories.ConnectionRepository;
 import com.recordcollection.recorddatabase.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
 
 @RestController
 @CrossOrigin
@@ -19,5 +30,23 @@ public class ConnectionController {
     @Autowired
     private CollectorRepository collectorRepository;
 
+    @GetMapping("/friends")
+    public ResponseEntity<?> getAllFriends() {
+        User currentUser = userService.getCurrentUser();
+
+        if (currentUser == null) {
+            return new ResponseEntity<>(new MessageResponse("Invalid user"), HttpStatus.BAD_REQUEST);
+        }
+
+        Collector developer = collectorRepository.findByUser_Id(currentUser.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        Set<Connection> rels = repository.findAllByOriginator_IdAndType(developer.getId(), EConnection.ACCEPTED);
+
+        Set<Connection> invRels = repository.findAllByRecipient_IdAndType(developer.getId(), EConnection.ACCEPTED);
+
+        rels.addAll(invRels);
+
+        return new ResponseEntity<>(rels, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/recordcollection/recorddatabase/repositories/CollectorRepository.java
+++ src/main/java/com/recordcollection/recorddatabase/repositories/CollectorRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.text.CollationElementIterator;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CollectorRepository extends JpaRepository<Collector, Long> {
     List<Collector> findAllByRecordsId(Long recordId, Sort sort);
+    Optional<Collector> findByUser_Id(Long id);
 }

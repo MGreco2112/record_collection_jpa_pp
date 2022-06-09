720932ad87d9919f31b8e3bb874ed57703371654
Matthew Greco <mgreco2112@tutanota.com>
2022-06-09 19:09:15 -0400

    finished basic oauth token setup

--- src/main/java/com/recordcollection/recorddatabase/controllers/DiscogsApiController.java
+++ src/main/java/com/recordcollection/recorddatabase/controllers/DiscogsApiController.java
@@ -1,5 +1,6 @@
 package com.recordcollection.recorddatabase.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,5 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RestController
 @RequestMapping("/api/discogs")
+@PreAuthorize("hasRole('ADMIN')")
 public class DiscogsApiController {
+
+
 }

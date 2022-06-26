92b31636ff3eb45012dc997b19edc9874ee659ff
Matthew Greco <mgreco2112@tutanota.com>
2022-06-26 16:28:55 -0400

    formatted response object for SearchRecords call

--- src/main/java/com/recordcollection/recorddatabase/payloads/api/response/DiscogsRecordSearchResponse.java
+++ src/main/java/com/recordcollection/recorddatabase/payloads/api/response/DiscogsRecordSearchResponse.java
@@ -1,11 +1,18 @@
 package com.recordcollection.recorddatabase.payloads.api.response;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscogsRecordSearchResponse {
     private String title;
+    private String artist;
     private String resource_url;
 
     public DiscogsRecordSearchResponse(String title, String resource_url) {
-        this.title = title;
+        String[] formattedTitle = title.split("-");
+
+        this.artist = formattedTitle[0].trim();
+        this.title = formattedTitle[1].trim();
         this.resource_url = resource_url;
     }
 
@@ -15,6 +22,14 @@ public class DiscogsRecordSearchResponse {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
     }
 
     public String getResource_url() {

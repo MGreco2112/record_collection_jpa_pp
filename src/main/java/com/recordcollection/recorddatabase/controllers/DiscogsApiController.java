aab82044f24b1343caac96668e569f72cf089f62
Matthew Greco <mgreco2112@tutanota.com>
2022-06-28 14:05:07 -0400

    updated finished method to account for conversion method call

--- src/main/java/com/recordcollection/recorddatabase/controllers/DiscogsApiController.java
+++ src/main/java/com/recordcollection/recorddatabase/controllers/DiscogsApiController.java
@@ -147,6 +147,7 @@ public class DiscogsApiController {
                     .asObject(DiscogsRecord.class)
                     .getBody();
 
+            discogsToRecordConversion(newRecord);
         }
 
 
